a687044c779be45ea4d015329d3b5142abe947b0
Michel-Alexandre <michel-alexandre.belanger@ville.terrebonne.qc.ca>
2026-02-10 22:23:12 -0500

    update

--- mp3-tag-json-src-react/src/player/Labels.jsx
+++ mp3-tag-json-src-react/src/player/Labels.jsx
@@ -1,9 +1,18 @@
 export function Labels(props) {
-  const { mp3RelativePath, expention } = props.mp3TagJson;
+  const { mp3TagJson } = props;
   return (
     <div>
-      mp3RelativePath : {mp3RelativePath} <br />
-      expention : {expention}
+      mp3RelativePath : {mp3TagJson.mp3RelativePath} <br />
+      "duration": {mp3TagJson.duration} <br />
+      "bpm": {mp3TagJson.bpm} <br />
+      "instrumentOrTypes": {mp3TagJson.instrumentOrTypes} <br />
+      "expention": {mp3TagJson.expention} <br />
+      "festive": {mp3TagJson.festive} <br />
+      "contact": {mp3TagJson.contact} <br />
+      "rythmic": {mp3TagJson.rythmic} <br />
+      "bass": {mp3TagJson.bass} <br />
+      "curve": {mp3TagJson.curve} <br />
+      "note": {mp3TagJson.note} <br />
     </div>
   );
 }
