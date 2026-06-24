0230c0ab7ed5f510e6d7e02e728ff4e7bf573ede
Michel-Alexandre <michel-alexandre.belanger@ville.terrebonne.qc.ca>
2026-06-24 10:15:43 -0400

    update

--- mp3-tag-json-src-react-single-html/src/utils/fileUtils.js
+++ mp3-tag-json-src-react-single-html/src/utils/fileUtils.js
@@ -6,6 +6,5 @@ export function getFilePath(relativePath) {
 
   const baseLocalPath = "file:///home/data/my_data/dev/dj-project/mp3-tag-json";
 
-  return "file:///home/data/my_data/dev/dj-project/mp3-tag-json/data/mp3/new01/Norah%20Jones%20-%20Don't%20Know%20Why.mp3";
-  // return baseLocalPath + relativePath;
+  return baseLocalPath + relativePath;
 }
