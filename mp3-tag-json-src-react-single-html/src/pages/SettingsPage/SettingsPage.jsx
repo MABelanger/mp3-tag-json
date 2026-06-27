745516b65569da2ef2252abe1e72f1cff69048c2
Michel-Alexandre <michel-alexandre.belanger@ville.terrebonne.qc.ca>
2026-06-27 00:33:45 -0400

    update

--- mp3-tag-json-src-react-single-html/src/pages/SettingsPage/SettingsPage.jsx
+++ mp3-tag-json-src-react-single-html/src/pages/SettingsPage/SettingsPage.jsx
@@ -1,15 +1,27 @@
 import { useLocation } from "react-router-dom";
+import { useWriteFile } from "../../components/ReadWriteDirectory/hooks/useWriteFile";
 
 export function SettingsPage(props) {
   const location = useLocation();
+  const { writeNestedFile, isSaving } = useWriteFile();
   const scannedFiles = location.state?.scannedFiles;
+  const dirHandle = location.state?.dirHandle;
 
+  function handleWrite() {
+    writeNestedFile(
+      dirHandle,
+      "mp3/new01/Norah Jones - Sunrise.mp3.txt",
+      "test"
+    );
+  }
   return (
     <div>
       <h1>Settings Page</h1>
-      scannedFiles, dirHandle
-      <div>{props.dirHandle}</div>
-      <p>Manage your account preferences here.</p>
+      {
+        //JSON.stringify(scannedFiles, null, 3)
+      }
+      dirHandle : {JSON.stringify(dirHandle, null, 3)}
+      <button onClick={handleWrite}> write </button>
     </div>
   );
 }
